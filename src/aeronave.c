6b77928208c8f850b0049f37bfc9c2d40638435f
clara lima <clarinha8643@gmail.com>
2026-03-26 21:37:40 -0300

    Fix create and print aircraft functions
    Fix function declaration and improve printing of aircraft details.

--- src/aeronave.c
+++ src/aeronave.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 #include "include/aeronave.h"
+// cria a função para cadastrar a aeronave
+aeronave criar\aeronave(int id,float,char código[],float x,float y,float altitude); {
+  aeronave a; // cria uma variável para a criação de uma aeronave
+  a.id=id; // atribui o id da aeronave
+  strcpy(a.codigo_voo, codigo); // copia o código do voo (string)
+  a.x; // posição x
+  a.y; // posição y
+  a.altitude = altitude; // altitude do avião
+  return a; // vai retornar a aeronave que foi criada
+}
+// imprimir as aeronaves
+void imprimirAeronave(Aeronave a) { 
+  printf("ID: %d\n" a.id); //mostra o id da aeronave
+  printf("codigo: %s\n" a.codigo_voo); // mostra o código do voo
+  printf("Posição: %2.f %2.f\n" , a.x a.y); //mostra a posição x e y
+  
+
+}
