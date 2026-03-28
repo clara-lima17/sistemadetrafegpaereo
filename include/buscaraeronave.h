c3e95ad744ac5f4ddbc9ee2032ea2b3a10e43c78
clara lima <clarinha8643@gmail.com>
2026-03-28 20:26:01 -0300

    Add Aeronave struct and findAeronave function

--- include/buscaraeronave.h
+++ include/buscaraeronave.h
@@ -1,1 +1,17 @@
+#ifndef buscar_h
+#define buscar_h
+
+// função que faz a modelagem
+typedef struct{
+    int id;
+    char codigo_voo[15];
+    float x,y;
+    float altitude;
+    float velocidade;
+    int status;
+
+} Aeronave;
+
+int findAeronave(Aeronave lista[] , int tamanho,char callsing []); // função em que procura a aeronave, o tamanho e o código dessa aeronave
+#endif
 
