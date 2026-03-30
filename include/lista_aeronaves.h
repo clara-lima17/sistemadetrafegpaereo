e554a636e0f1ba8e4e3f7e34fcb3025f1afff5f0
clara lima <clarinha8643@gmail.com>
2026-03-30 11:16:19 -0300

    Add findAeronave function prototype to lista_aeronaves.h

--- include/lista_aeronaves.h
+++ include/lista_aeronaves.h
@@ -20,5 +20,6 @@ int addAeronave(ListaAeronaves *lista);
 // Altera o status de uma aeronave pelo ID
 // status: 0 = voando, 1 = pousando, 2 = aguardando
 int alterarStatusAeronave(ListaAeronaves *lista, int id, int novoStatus);
+int findAeronave(Aeronave lista[] , int tamanho,char callsing []); // função em que procura a aeronave, o tamanho e o código dessa aeronave
 
 #endif 
